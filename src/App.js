2cb2992eaa8d4c1293adfb3d74ce403f27655983
Ben Nguyen <nguyenkhacly2612@gmail.com>
2026-01-20 16:49:35 +0700

    ben commit lan 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { useState, Fragment } from 'react';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <h1>Welcome to Ben's project</h1>
+                <ul>
+                    {publicRoutes.map((route, index) => {
+                        const pageName = route.catagoryName;
+                        return (
+                            <li key={index}>
+                                <NavLink
+                                    className={({ isActive }) => {
+                                        return isActive ? 'activeCatagory' : undefined;
+                                    }}
+                                    to={route.path}
+                                >
+                                    {pageName}
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Page = route.component;
+                    let Layout = DefaultLayout;
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+import Home from '~/Pages/Home';
+import Following from '~/Pages/Following';
+import Profile from '~/Pages/Profile';
+import Upload from '~/Pages/Upload';
+import Search from '~/Pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+
+const publicRoutes = [
+    {
+        path: '/',
+        catagoryName: 'Home',
+        component: Home,
+    },
+    {
+        path: '/following',
+        catagoryName: 'Following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        catagoryName: 'Profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        catagoryName: 'Upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/Search',
+        catagoryName: 'Search',
+        component: Search,
+        layout: null,
+    },
+];
+
+const privateRoutes = {};
+
+export { publicRoutes, privateRoutes };
